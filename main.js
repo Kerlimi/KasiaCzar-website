88fffa7c222bd8cd914efd7f177edf66b1135a2d
kerlimi <kasiaczarczynska@gmail.com>
2022-09-26 14:08:28 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+const cards = [...document.querySelectorAll(".card_inner")];
+
+cards.forEach((element) => {
+  element.addEventListener("click", clickCard);
+});
+
+function clickCard(e) {
+  if (!e.target.classList.contains("card_inner")) return;
+  e.target.classList.toggle("is-flipped");
+}
